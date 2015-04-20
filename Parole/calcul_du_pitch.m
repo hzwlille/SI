56ab1cba792907972d5c1e1ffe7c6990212392b8
HU Zhongwei <kxyh@qq.com>
2015-04-20 23:06:06 +0200

    good

--- Parole/calcul_du_pitch.m
+++ Parole/calcul_du_pitch.m
@@ -1,7 +1,15 @@
-function [ pitch ] = calcul_du_pitch( vecteur, pitch_min, pitch_max )
+function [ pitch ,a] = calcul_du_pitch( vecteur, pitch_min, pitch_max )
 %CALCUL_DU_PITCH Summary of this function goes here
 %   Detailed explanation goes here
-cov(vecteur);
+auto_vec=xcov(vecteur);
+min=floor(8000/pitch_max/2);
+find_indice=auto_vec((min+size(vecteur)):end);
+[~,indice_Max]=max(find_indice);
+pitch=floor(8000/(indice_Max+min));
 
+if (pitch<pitch_min || pitch>pitch_max)
+    pitch=pitch_min;
+end 
+  a=pitch;  
 end
 
